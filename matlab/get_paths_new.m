5c1f1c6bd0b3285606ba5fe92702748b49e93e1f
thayer <thayer@terrier.rit.edu>
2019-05-31 11:43:37 -0400

    updated the name to be npaths and such

--- matlab/get_paths_new.m
+++ matlab/get_paths_new.m
@@ -1,16 +1,16 @@
-function paths=get_paths()
-   paths.datadir = '/data/symons/nh_data/mat/';
-   paths.imagedir = '/data/symons/nh_data/regist/selected_data/goodplus/';
-   paths.astrodir = '/data/symons/nh_data/regist/goodplus/';
-   paths.catdir = '/data/symons/nh_data/usnob1/mat_files/';
-   paths.maskdir = '/data/symons/nh_data/masks/';
-   paths.ghostdir = '/data/symons/nh_data/ghosts/';
-   %paths.darkfitsdir = '/data/zemcov/NH/NH/launch/dark/selected_data/';
-   %paths.darkdir = '/data/zemcov/NH/dark/';
-   paths.engdir = '/data/symons/nh_data/eng_data/';
-   %paths.tridir = '/data/zemcov/NH/trilegal/';
-   paths.mandir = '/data/symons/nh_data/manmask/';
-   %paths.gscdir = '/data/zemcov/NH/GSCII/';
-   %paths.irisdir = '/data/zemcov/NH/IRIS/';
+function npaths=get_paths_new()
+   npaths.datadir = '/data/symons/nh_data/mat/';
+   npaths.imagedir = '/data/symons/nh_data/regist/selected_data/goodplus/';
+   npaths.astrodir = '/data/symons/nh_data/regist/goodplus/';
+   npaths.catdir = '/data/symons/nh_data/usnob1/mat_files/';
+   npaths.maskdir = '/data/symons/nh_data/masks/';
+   npaths.ghostdir = '/data/symons/nh_data/ghosts/';
+   %npaths.darkfitsdir = '/data/zemcov/NH/NH/launch/dark/selected_data/';
+   %npaths.darkdir = '/data/zemcov/NH/dark/';
+   npaths.engdir = '/data/symons/nh_data/eng_data/';
+   %npaths.tridir = '/data/zemcov/NH/trilegal/';
+   npaths.mandir = '/data/symons/nh_data/manmask/';
+   %npaths.gscdir = '/data/zemcov/NH/GSCII/';
+   %npaths.irisdir = '/data/zemcov/NH/IRIS/';
    
 end
