8d7d141a10bef5eb9ab9030d051a792349c904a0
Ben Vaughan <vaughanb18@gmail.com>
2020-01-27 14:04:35 -0500

    fixed output RA and DEC fields

--- py/iris/nh_iris_map.py
+++ py/iris/nh_iris_map.py
@@ -19,7 +19,6 @@ from astropy.wcs import WCS as world
 from astropy.wcs.utils import pixel_to_skycoord, skycoord_to_pixel
 import matplotlib.pyplot as plt
 
-
 #choose the field that you want
 field = config.fields[:,4] # : selects both RA and Dec, the 4 denotes the number
 # i.e. position in the txt file, 4 means the fourth from the top
@@ -36,9 +35,10 @@ naxis = int(sqrt(2.) * 256)
 naxis1 = naxis2 = naxis
 
 #holder for the x, y pixel coordinates that we want.
-xvec = np.arange(0, naxis1)
-yvec = np.arange(0, naxis2)
-
+x = np.arange(0, naxis1)
+y = np.arange(0, naxis2)
+xvec = np.repeat(x[:, np.newaxis], naxis2, axis=1)
+yvec = np.repeat(y[np.newaxis, :], naxis1, axis=0)
 head = make_header(pixsize, naxis, ra, dec) #this function assumes a square.
 
 w = world(head)
@@ -51,17 +51,23 @@ dec = np.asarray(c.dec.to_string(decimal=True), dtype=float)
 map = mosaic(head, band=4)
 
 #for debugging
-plt.imshow(map, origin='lower')
-plt.show()
+# plt.imshow(map, origin='lower')
+# plt.show()
 
+#convert field to a string for filenames
+field = str(field[0]) + ',' + str(field[1])
+
+#image
 hdu = fits.PrimaryHDU(map, head)
 hdul = fits.HDUList([hdu])
-hdul.writeto(config.DataDir + str(field) + '_fx.fits')
+hdul.writeto(config.DataDir + field + '_fx.fits')
 
+#ra
 hdu = fits.PrimaryHDU(ra, head)
 hdul = fits.HDUList([hdu])
-hdul.writeto(config.DataDir + str(field) + '_ra.fits')
+hdul.writeto(config.DataDir + field + '_ra.fits')
 
+#dec
 hdu = fits.PrimaryHDU(dec, head)
 hdul = fits.HDUList([hdu])
-hdul.writeto(config.DataDir + str(field) + '_dc.fits')
+hdul.writeto(config.DataDir + field + '_dc.fits')
